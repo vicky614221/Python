8ba94514001f2724fb8ee91cace0feed8bd73d51
vicky <vickygreat96@gmail.com>
2024-11-25 01:23:07 +0530

    changes made

--- Hospital/Hospital_portal_GUI.py
+++ Hospital/Hospital_portal_GUI.py
@@ -29,44 +29,46 @@ button_admin_portal.grid(row=1,column=2,rowspan=3,sticky='nsew',pady=50,padx=10)
 window_frame.grid(row=0,column=0,sticky='nsew',padx=5,pady=5)
 
 def open_specific_portal(user):
-    global specific_portal
-    specific_portal = tk.Toplevel()
-    specific_portal.grab_set()
-    specific_portal.minsize(width=300, height=300)
+    global window_specific_portal
+    window_specific_portal = tk.Toplevel()
+    window_specific_portal.grab_set()
+    window_specific_portal.minsize(width=300, height=300)
     #specific_portal.geometry('300x300+300+300')
-    specific_portal.rowconfigure(0,weight=1)
-    specific_portal.columnconfigure(0,weight=1)
-    specific_portal_frame = ttk.Frame(master=specific_portal,borderwidth=20)
-    specific_portal_frame.rowconfigure((0,1,2,3,4),weight=1)
-    specific_portal_frame.columnconfigure((0,1,2),weight=1)
+    window_specific_portal.rowconfigure(0, weight=1)
+    window_specific_portal.columnconfigure(0, weight=1)
+    global frame_specific_portal
+    frame_specific_portal = ttk.Frame(master=window_specific_portal)
+    #frame_specific_portal_frame = ttk.Frame(master=specific_portal,borderwidth=20)
+    frame_specific_portal.rowconfigure((0,1,2,3,4),weight=1)
+    frame_specific_portal.columnconfigure((0,1,2),weight=1)
     if user == 'P':
-        label_patient_heading = ttk.Label(master=specific_portal_frame,text='PATIENT PORTAL LOGIN',background='red',font='Ariel,40,bold')
+        label_patient_heading = ttk.Label(master=frame_specific_portal,text='PATIENT PORTAL LOGIN',background='red',font='Ariel,40,bold')
         label_patient_heading.grid(row=0,column=0,columnspan=3,pady=10)
         global rbutton_select_str
         rbutton_select_str = tk.StringVar()
-        rbutton_select1 = ttk.Radiobutton(master=specific_portal_frame,text='First time user?',variable=rbutton_select_str,value='new',command=new_patient_selected)
+        rbutton_select1 = ttk.Radiobutton(master=frame_specific_portal,text='First time user?',variable=rbutton_select_str,value='new',command=new_patient_selected)
         rbutton_select1.grid(row=1,column=0)
         #rbutton_select_str = tk.StringVar()
-        rbutton_select2 = ttk.Radiobutton(master=specific_portal_frame,text='Existing user?',variable=rbutton_select_str,value='existing',command=exist_patient_selected)
+        rbutton_select2 = ttk.Radiobutton(master=frame_specific_portal,text='Existing user?',variable=rbutton_select_str,value='existing',command=exist_patient_selected)
         rbutton_select2.grid(row=1,column=2)
         global entry_id_str
         entry_id_str = tk.StringVar(value='Enter patient ID')
         global entry_id
-        entry_id = tk.Entry(master=specific_portal_frame,textvariable=entry_id_str,state='disabled')
+        entry_id = tk.Entry(master=frame_specific_portal,textvariable=entry_id_str,state='disabled')
         entry_id.grid(row=2,column=0,columnspan=3,sticky='ew')
         global entry_pwd_str
         entry_pwd_str = tk.StringVar(value='Enter your password')
         global entry_pwd
-        entry_pwd = tk.Entry(master=specific_portal_frame,textvariable=entry_pwd_str,show='*',state='disabled')
+        entry_pwd = tk.Entry(master=frame_specific_portal,textvariable=entry_pwd_str,show='*',state='disabled')
         entry_pwd.grid(row=3,column=0,columnspan=3,sticky='ew')
         global button_submit
-        button_submit = ttk.Button(master=specific_portal_frame,text='SUBMIT',state='disabled',command=lambda :validate_and_show(user))
+        button_submit = ttk.Button(master=frame_specific_portal,text='SUBMIT',state='disabled',command=lambda :validate_and_show(user))
         button_submit.grid(row=4,column=0,columnspan=3,)
     elif user == 'D':
         pass
     else:
         pass
-    specific_portal_frame.grid(row=0,column=0,padx=10,pady=10,sticky='nsew')
+    frame_specific_portal.grid(row=0,column=0,padx=10,pady=10,sticky='nsew')
 def exist_patient_selected():
     if rbutton_select_str.get() == 'existing':
         entry_id.configure(state='normal')
@@ -78,7 +80,7 @@ def new_patient_selected():
         entry_pwd.configure(state='disabled')
         button_submit.configure(state='disabled')
         global window_np
-        window_np = tk.Toplevel(master=specific_portal)
+        window_np = tk.Toplevel(master=window_specific_portal)
         window_np.grab_set()
         window_np.minsize(width=300,height=300)
         window_np.title('New Patient Registration')
@@ -157,27 +159,37 @@ def show_patient_det():
 def validate_and_show(user):
     if user == 'P':
         patient_obj = PortalUser(user='P', id=entry_id_str.get(),name=None, dob=None, gender=None,email=None,phone_no=None,aadhar=None,address=None)
-        patient_obj.is_valid_user('P',entry_id_str.get(),entry_pwd_str.get())
-        patient_id,patient_name,patient_dob,patient_gen,patient_email,patient_phone,patient_aadhar,patient_addr = patient_obj.get_portal_user_info()
-        #add logic to get appointment details
-
-        window_show_p = tk.Toplevel()
-        window_show_p.title('Patient Options')
-        window_show_p.minsize(width=300,height=300)
-        window_show_p.grab_set()
-        window_show_p.rowconfigure((0,1,2,3),weight=1)
-        window_show_p.columnconfigure((0,1),weight=1)
-        label_p_heading = ttk.Label(master=window_show_p,text=('Hello '+ patient_name.upper()),font=('Times New Roman', 15, 'bold'),foreground='blue')
-        label_p_heading.grid(row=0,column=0,columnspan=2,padx=10,pady=10)
-        button_book_apt = ttk.Button(master=window_show_p,text='Book an appointment')
-        button_book_apt.grid(row=1,column=0,columnspan=2,sticky='nsew',padx=10,pady=10)
-        button_check_apt = ttk.Button(master=window_show_p, text='Check next appointment')
-        button_check_apt.grid(row=2, column=0, columnspan=2,sticky='nsew',padx=10,pady=10)
-        button_edit_per = ttk.Button(master=window_show_p, text='Edit personal details',command=edit_personal_det)
-        button_edit_per.grid(row=3, column=0, columnspan=2,sticky='nsew',padx=10,pady=10)
+        if patient_obj.is_valid_user('P',entry_id_str.get(),entry_pwd_str.get()):
+            patient_id,patient_name,patient_dob,patient_gen,patient_email,patient_phone,patient_aadhar,patient_addr = patient_obj.get_portal_user_info()
+            #add logic to get appointment details
+            frame_specific_portal.grid_forget()
+            #window_show_p = tk.Toplevel()
+            #window_show_p.title('Patient Options')
+            #window_show_p.minsize(width=300,height=300)
+            #window_show_p.grab_set()
+            #window_show_p.rowconfigure((0,1,2,3),weight=1)
+            #window_show_p.columnconfigure((0,1),weight=1)
+            global frame_show_p
+            frame_show_p = ttk.Frame(master=window_specific_portal)
+            frame_show_p.columnconfigure((0,1),weight=1)
+            frame_show_p.rowconfigure((0, 1, 2, 3, 4), weight=1)
+            label_p_heading = ttk.Label(master=frame_show_p,text=('Hello '+ patient_name.upper()),font=('Times New Roman', 15, 'bold'),foreground='blue')
+            label_p_heading.grid(row=0,column=0,columnspan=2,padx=10,pady=10)
+            button_book_apt = ttk.Button(master=frame_show_p,text='Book an appointment')
+            button_book_apt.grid(row=1,column=0,columnspan=2,sticky='nsew',padx=10,pady=10)
+            button_check_apt = ttk.Button(master=frame_show_p, text='Check next appointment')
+            button_check_apt.grid(row=2, column=0, columnspan=2,sticky='nsew',padx=10,pady=10)
+            button_edit_per = ttk.Button(master=frame_show_p, text='Edit personal details',command=edit_personal_det)
+            button_edit_per.grid(row=3, column=0, columnspan=2,sticky='nsew',padx=10,pady=10)
+            button_back_patient_portal = ttk.Button(master=frame_show_p,text='Go back',command=go_back_to_patient_portal)
+            button_back_patient_portal.grid(row=4,column=1)
+            frame_show_p.grid(row=0,column=0)
 
 def edit_personal_det():
     tkinter.messagebox.showinfo(title='ACCESS DENIED',message='Please reach out to hospital admin team to edit details')
 
+def go_back_to_patient_portal():
+    frame_show_p.grid_forget()
+    frame_specific_portal.grid(row=0,column=0,padx=10,pady=10,sticky='nsew')
 
 window.mainloop()
