a9c1e6da190e7ab05ba5afacb424698bf5ff0225
vicky614221 <vickygreat96@gmail.com>
2025-02-13 00:01:09 +0530

    small change

--- Py/hello_world.py
+++ Py/hello_world.py
@@ -15,25 +15,29 @@ for i in name_2:
         continue
     else:
         name2_list.append(i)
-for i in name1_list:
-    for j in name2_list:
-        if i == j:
-            name1_list.remove(i)
-            name2_list.remove(j)
-            break
-print(name1_list)
-print(name2_list)
-flames = ['Friend','Lover','Affectionate','Marriage','Enemy','Sister']
-flames_cnt = len(name1_list) + len(name2_list)
 
-if flames_cnt > 0:
-    while len(flames)>0:
-        (flames.pop((flames_cnt % len(flames)) - 1))
-        if len(flames) == 1:
-            print(flames)
+for cnt in range(len(name1_list)):
+    break_flag_yes = False
+    for i in name1_list:
+        for j in name2_list:
+            if i == j:
+                name1_list.remove(i)
+                name2_list.remove(j)
+                break_flag_yes = True
+                break
+        if break_flag_yes:
             break
+flame_cnt = len(name1_list) + len(name2_list)
+flames = ['Friend','Lover','Affection','Marriage','Enemy','Sister/Brother']
+
+if flame_cnt > 0:
+    while len(flames) > 1:
+        flames.pop(flame_cnt-1)
+        continue
 else:
     print('All letters are matching')
+print(flames)
+
 
 
 
